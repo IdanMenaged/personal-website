11e3a9f7ef650f644a4a4c9c57a19b2cfc07760c
Idan Menaged <idanmenaged@gmail.com>
2024-11-22 16:55:38 +0200

    added easter egg

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function onCmdSubmit(event) {
     const responsesEl = document.getElementById("responses")
     const responses = new Map([
-        ["help", "goto &lt;page>"]
+        ["help", "goto &lt;page>"],
+        ["goto", "site is still under construction. no pages are added yet"],
+        ["easteregg", "well well well look who's looking at the code. hopefully you haven't found all the little" +
+        "secrets here..."],
     ])
 
     if (event.key === "Enter") {
-        const cmd = event.target.value
+        const [cmd, ...params] = event.target.value.split(' ')
         if (responses.has(cmd)) {
             responsesEl.innerHTML += `<p>${responses.get(cmd)}</p>`
         }
