64ccdfbba45de5676d8277c854a8da88440acea5
Idan Menaged <idanmenaged@gmail.com>
2024-11-22 17:33:50 +0200

    function logic

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 function onCmdSubmit(event) {
     const responsesEl = document.getElementById("responses")
-    const responses = new Map([
-        ["help", "goto &lt;page> go to a page<br>" +
-        "list-pages list all pages"],
-        ["goto", "site is still under construction. no pages are added yet"],
-        ["easteregg", "well well well look who's looking at the code. hopefully you haven't found all the little" +
-        "secrets here..."],
-        ["list-pages", "site is under construction, pages coming soon!"]
-    ])
 
     if (event.key === "Enter") {
         const [cmd, ...params] = event.target.value.split(' ')
-        if (responses.has(cmd)) {
-            responsesEl.innerHTML += `<p>${responses.get(cmd)}</p>`
+        const funcName = toCamelCase(`handle-${cmd}`)  // capitalise first letter of cmd
+        if (typeof window[funcName] === "function") {
+            responsesEl.innerHTML += `<p>${window[funcName]([params])}</p>`
         }
         else {
             responsesEl.innerHTML += `<p>unrecognized command. use 'help'.</p>`
         }
     }
+}
+
+function toCamelCase(s) {
+    return s
+        .replace(/([-_][a-z])/gi, (match) => match.toUpperCase()
+            .replace('-', '')
+            .replace('_', '')
+        );
+}
+
+function handleHelp() {
+    return "goto &lt;page> go to a page<br>" +
+        "list-pages list all pages"
+}
+
+function handleGoto(page) {
+    window.location.href = `pages/${page}/${page}.html`
+    return "loading..."
+}
+
+function handleListPages() {
+    return "languages"
+}
+
+function handleEasteregg() {
+    return "well well well look who's looking for secrets. hopefully you haven't found everything i've hidden here" +
+        "yet..."
 }
